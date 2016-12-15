89033dad21193b72ae00d9d5964bfc795384e78f
AnHardt <github@kitelab.de>
2016-12-15 12:50:22 +0100

    Optimize handling of block_buffer_runtime()
    millis_t is long - divisions take for ever.
    
    Return a kind of millisecond instead of microsecond -
    divided by 1024 instead of 1000 for speed. (2.4% error)
    
    That does not matter because block_buffer_runtime is
    already a too short estimation.
    Shrink the return-type.

--- Marlin/planner.h
+++ Marlin/planner.h
@@ -395,10 +395,16 @@ class Planner {
 
     #if ENABLED(ULTRA_LCD)
 
-      static millis_t block_buffer_runtime() {
+      static uint16_t block_buffer_runtime() {
         CRITICAL_SECTION_START
           millis_t bbru = block_buffer_runtime_us;
         CRITICAL_SECTION_END
+        // To translate µs to ms a division by 1000 would be required.
+        // We introduce 2.4% error her by dividing by 1024.
+        // Does not matter because block_buffer_runtime_us is already an, too small, estimation.
+        bbru >>= 10;
+        // limit to about a minute.
+        NOMORE(bbru, 0xfffful);
         return bbru;
       }
 
